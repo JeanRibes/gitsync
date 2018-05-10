17c1bbda79f23faa6eb4a9fb0dbefb859f76c48f
Jean Ribes <jean.christophe.ribes@sfr.fr>
2018-05-10 14:52:46 +0200

    la CI/CD ghetto ne dépend plus de l'adresse

--- fetch.php
+++ fetch.php
@@ -1,5 +1,5 @@
 <html><head>
-<meta http-equiv="refresh" content="0; URL=https://test.ribes.me/reset.php">
+<meta http-equiv="refresh" content="0; URL=/reset.php">
 </head>
 <body>
 <?php
--- reset.php
+++ reset.php
@@ -1,4 +1,4 @@
-<html><head><meta http-equiv="refresh" content="0; URL=https://test.ribes.me/git.php"></head>
+<html><head><meta http-equiv="refresh" content="0; URL=/git.php"></head>
 <body>
 <?php
 echo `git reset --hard origin/master`;

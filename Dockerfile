b20571a1d674fdbde051d9f1c7e7e05f46a2a15e
jean <jean@ribes.me>
2018-05-10 21:45:13 +0200

    dockerisation qui marche

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM trafex/alpine-nginx-php7
 
-RUN apk --update add git
 COPY initrepo.sh /initrepo.sh
-CMD /initrepo.sh
+COPY app /var/www/html/git
+RUN apk --update add git && chmod a+x /initrepo.sh
+CMD /initrepo.sh && /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
--- initrepo.sh
+++ initrepo.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 cd /var/www/html
+echo 'cloning '$GITHUB_URL' in '$PWD
+read
 git init
 git remote add origin $GITHUB_URL.git
 git pull origin master
 git branch --set-upstream-to=origin/master master
 chown -R nobody:nobody .
+rm index.php
+touch supervisord.log
+chmod a-r supervisord.log

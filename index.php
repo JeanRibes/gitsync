fdf53aac22d04878fe4f8440d503c960b5fb022b
Jean Ribes <jean.christophe.ribes@sfr.fr>
2018-05-10 14:19:35 +0200

    infos

--- index.php
+++ index.php
@@ -5,5 +5,10 @@
 		<h1>Bonjour</h1>
 		<p>Ce site est un test qui devrait pull automatiquement depuis github</p>
 		<a href="/fetch.php">Mettre à jour manuellement</a>
+		<footer>
+		<?php
+		   echo `git log --pretty="<a href='https://github.com/BdEINSALyon/site-dev/commit/%H'>%s</a>"|head -n 1`;
+		?>
+		</footer>
 	</body>
 </html>
